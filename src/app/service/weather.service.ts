c3d94b5fa839c30925abdffb3cbf6e0ac31db154
juanibarguen <h===>
2023-06-03 19:25:44 -0300

    creacion de servicio y conexion con la API

---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  // Ejemplo probado en Postman
+  // https://api.openweathermap.org/data/2.5/weather?q=London&appid=a3affa3e3df25f10c609f877595f4d82
+
+  private apiKey = 'a3affa3e3df25f10c609f877595f4d82'
+
+  constructor(private http: HttpClient) { }
+
+  getData(city: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`);
+  }
+
+}
