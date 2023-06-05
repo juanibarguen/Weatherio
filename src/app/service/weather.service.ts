c9c1312853b8b6cdfcc6f18cfef6e6c6bf7141b5
juanibarguen <h===>
2023-06-05 01:55:25 -0300

    se realizaron pruebas y se establecio la conexion con el endpoint 'forecast' de la API

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -9,13 +9,21 @@ export class WeatherService {
 
   // Ejemplo probado en Postman
   // https://api.openweathermap.org/data/2.5/weather?q=London&appid=a3affa3e3df25f10c609f877595f4d82
+  // https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=a3affa3e3df25f10c609f877595f4d82
+
 
   private apiKey = 'a3affa3e3df25f10c609f877595f4d82'
 
   constructor(private http: HttpClient) { }
 
-  getData(city: string): Observable<any> {
+  currentWeather(city: string): Observable<any> {
     return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`);
   }
+
+  forecast(city:string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${this.apiKey}`);
+  }
+
+
 
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -12,39 +12,73 @@ export class WeatherComponent implements OnInit{
   currentDate: Date = new Date();
   city!:string;
   weatherData: any = {};
+  forecastData: any[] = [];
   formattedDateEnUs:string | undefined;
   formattedDateEs:string | undefined;
+
 
   constructor( private apiService: WeatherService) {}
 
   ngOnInit(): void {
     // obtenemos la fecha actual en el formato indicado: 'Sunday 4, Jun'
+    // EN
     this.formattedDateEnUs = format(this.currentDate, "EEEE d, MMM", { locale: enUS });
+    // ES
     this.formattedDateEs = format(this.currentDate, "EEEE d, MMM", { locale: es });
-
-
   }
 
-  convertToCelsius(tempKelvin: number): number {
-    return tempKelvin - 273.15;
-  }
-
-  convertToFahrenheit(tempKelvin: number): number {
-    return Math.floor((tempKelvin - 273.15) * (9/5) + 32);
-  }
-  
   getWeather() {
-    this.apiService.getData(this.city).subscribe(data => {
+    this.apiService.currentWeather(this.city).subscribe(data => {
       this.weatherData = data;
-      console.log(data);
+      // console.log(data);
 
       if (this.weatherData.main && this.weatherData.main.temp) {
         const tempCelsius = this.convertToCelsius(this.weatherData.main.temp);
         this.weatherData.main.temp = Math.round(tempCelsius);
         // this.weatherData.main.tempFahrenheit = Math.floor(this.convertToFahrenheit(this.weatherData.main.temp)); 
       }
+      console.log(data);
     });
     this.city = ''; 
   }
+
+  getForecast() {
+    if (this.city) {
+      this.apiService.forecast(this.city).subscribe(data => {
+        this.forecastData = data.list.slice(0, 5).map((item: any) => ({
+          main: item.weather[0].main,
+          temp: item.main.temp
+        }));
+        console.log(this.forecastData);
+      });
+    }
+  }
+
+  executeFunctions() {
+    this.getForecast();
+    this.getWeather();
+  }
+
+  getNextDate(index: number): Date {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + index + 1);
+    return currentDate;
+  }
+
+  convertToCelsius(tempKelvin: number): number {
+    const tempCelsius = tempKelvin - 273.15;
+    return Math.floor(tempCelsius);
+  }
+
+  convertToFahrenheit(tempKelvin: number): number {
+    return Math.floor((tempKelvin - 273.15) * (9/5) + 32);
+  }
+
+  metersToKilometers(meters: number) {
+    var kilometers = meters / 1000;
+    return kilometers;
+  }
+  
+
 
 }
