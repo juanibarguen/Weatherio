2064fdcac3fc14414386174ff344df5930221abe
juanibarguen <h===>
2023-06-05 17:05:49 -0300

    se realizo la conexion con el endpoint de la api que retorna los datos de la contaminacion del aire en tiempo real, se utilizo la interpolacion para mostrar el valor de sunrise y sunset

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,7 @@ export class WeatherService {
 
 
   private apiKey = 'a3affa3e3df25f10c609f877595f4d82'
+  city: string | undefined
 
   constructor(private http: HttpClient) { }
 
@@ -24,5 +25,29 @@ export class WeatherService {
     return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${this.apiKey}`);
   }
 
+  // airpollution(city: string): Observable<any> {
+  //   return this.currentWeather(city).pipe(
+  //     switchMap((weatherData: any) => {
+  //       const lat = weatherData.coord.lat;
+  //       const lon = weatherData.coord.lon;
+  //       const start = /* valor del timestamp de inicio */;
+  //       const end = /* valor del timestamp de fin */;
+  //       return this.http.get(`http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${lat}&lon=${lon}&start=${start}&end=${end}&appid=${this.apiKey}`);
+  //     })
+  //   );
+  // }
+
+  airpollution(city: string): Observable<any> {
+    return this.currentWeather(city).pipe(
+      switchMap((weatherData: any) => {
+        const lat = weatherData.coord.lat;
+        const lon = weatherData.coord.lon;
+        
+        return this.http.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
+      })
+    );
+  }
+  
+  
 
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,28 +10,47 @@ import { enUS, es } from 'date-fns/locale';
 })
 export class WeatherComponent implements OnInit{
   currentDate: Date = new Date();
+  cityName:any
   city!:string;
   weatherData: any = {};
   forecastData: any[] = [];
   forecastDataHours: any[] = [];
   formattedDateEnUs:string | undefined;
   formattedDateEs:string | undefined;
-
-
+   datosContaminacionAire: any;
+   sunriseTime: string | undefined;
+  sunsetTime: string | undefined;
+  description: string | undefined
   constructor( private apiService: WeatherService) {}
 
+
+
   ngOnInit(): void {
+
+
     // obtenemos la fecha actual en el formato indicado: 'Sunday 4, Jun'
     // EN
     this.formattedDateEnUs = format(this.currentDate, "EEEE d, MMM", { locale: enUS });
     // ES
     this.formattedDateEs = format(this.currentDate, "EEEE d, MMM", { locale: es });
+
+
   }
+
+  getTimeFromUnix(unixTime: number): string {
+    const date = new Date(unixTime * 1000);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    
+    return `${hours}:${minutes}`;
+  }
+
 
   getWeather() {
     this.apiService.currentWeather(this.city).subscribe(data => {
       this.weatherData = data;
       // console.log(data);
+      this.description = this.weatherData.weather[0].description
 
       if (this.weatherData.main && this.weatherData.main.temp) {
         const tempCelsius = this.convertToCelsius(this.weatherData.main.temp);
@@ -39,9 +58,21 @@ export class WeatherComponent implements OnInit{
         // this.weatherData.main.tempFahrenheit = Math.floor(this.convertToFahrenheit(this.weatherData.main.temp)); 
       }
       console.log(data);
+  
+        if (this.weatherData.sys && this.weatherData.sys.sunrise && this.weatherData.sys.sunset) {
+          this.sunriseTime = this.getTimeFromUnix(this.weatherData.sys.sunrise);
+          this.sunsetTime = this.getTimeFromUnix(this.weatherData.sys.sunset);
+          console.log('Sunrise:', this.sunriseTime);
+          console.log('Sunset:', this.sunsetTime);
+        }
     });
+    this.obtenerContaminacionAire(this.city);
+  
     this.city = ''; 
+
+    
   }
+  
 
   getForecast() {
     if (this.city) {
@@ -56,39 +87,6 @@ export class WeatherComponent implements OnInit{
   }
 
  
-//  getForecastHours() {
-//     if (this.city) {
-//       this.apiService.forecast(this.city).subscribe(data => {
-//         const currentDate = new Date(); // Obtiene la fecha y hora actual
-//         const currentHour = currentDate.getHours(); // Obtiene la hora actual
-//         var nextMultipleOfThree = Math.ceil(currentHour / 3) * 3; // Calcula la próxima hora múltiplo de 3
-        
-//         let count = 0;
-//         for (let i = 0; i < data.list.length; i++) {
-//           const forecastDate = new Date(data.list[i].dt_txt); // Obtiene la fecha y hora del pronóstico
-          
-//           // Verifica si la hora del pronóstico coincide con la hora calculada
-//           if (forecastDate.getHours() === nextMultipleOfThree) {
-//             this.forecastDataHours.push({
-//               main: data.list[i].weather[0].main,
-//               temp: data.list[i].main.temp
-//             });
-            
-//             count++;
-//             const nextHour = nextMultipleOfThree + 3 * count; // Calcula la próxima hora múltiplo de 3 para el próximo pronóstico
-//             nextMultipleOfThree = nextHour >= 24 ? nextHour - 24 : nextHour; // Ajusta la próxima hora si supera las 24 horas
-            
-//             // Verifica si se han obtenido exactamente 8 pronósticos
-//             if (count >= 8) {
-//               break;
-//             }
-//           }
-//         }
-        
-//         console.log(this.forecastDataHours);
-//       });
-//     }
-//   }
   
 getForecastHours() {
   if (this.city) {
@@ -129,7 +127,18 @@ getForecastHours() {
   }
 }
 
-
+obtenerContaminacionAire(ciudad: string) {
+  this.apiService.airpollution(ciudad).subscribe(
+    (data: any) => {
+      this.datosContaminacionAire = data; // Asignar los datos a la variable
+      console.log(data);
+      
+    },
+    (error: any) => {
+      console.error(error);
+    }
+  );
+}
   
   
   
@@ -161,6 +170,18 @@ getForecastHours() {
     return kilometers;
   }
   
-
+  // capitalizeFirstLetterOfEachWord(text: string): string {
+  //   const words = text.split(' ');
+  
+  //   const capitalizedWords = words.map(word => {
+  //     const firstLetter = word.charAt(0).toUpperCase();
+  //     const restOfWord = word.slice(1);
+  //     return firstLetter + restOfWord;
+  //   });
+  
+  //   return capitalizedWords.join(' ');
+  // }
+  
+  
 
 }
