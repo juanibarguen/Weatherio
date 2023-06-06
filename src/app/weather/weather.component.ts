7383b846a04bbc8037f18c606ae9dc02f606b051
juanibarguen <h===>
2023-06-06 03:30:23 -0300

    agregamos medias querys para el responsive completo

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -13,6 +13,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherComponent implements OnInit{
 
+  off = false
   currentDate: Date = new Date();
 
   control = new FormControl();
